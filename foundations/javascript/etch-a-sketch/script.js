792e8e5306020a4d3313d5bf690a7b267f26f0ea
rseymour <rjs.github@gmail.com>
2023-10-12 22:46:23 -0400

    completed etch-a-sketch

--- foundations/javascript/etch-a-sketch/script.js
+++ foundations/javascript/etch-a-sketch/script.js
@@ -1,21 +1,62 @@
 const container = document.querySelector('.grid-container');
+const button = document.querySelector('button');
 
-let sideLength = 800;
-let cellsPerRow = 64;
+// setup grid size
+let gridSideLength = 960;
 
-container.style.width = `${sideLength}px`;
-container.style.height = `${sideLength}px`;
-container.style.gridTemplateColumns = `repeat(${cellsPerRow}, 1fr)`;
-container.style.gridTemplateRows = `repeat(${cellsPerRow}, 1fr)`;
+container.style.width = `${gridSideLength}px`;
+container.style.height = `${gridSideLength}px`;
 
-let createDivs = (num) => {
-    for (let i = 0; i < num; i++) {
-        const div = document.createElement('div');
-        div.classList.add('cell');
-        container.appendChild(div);
-    }
+
+let getRandomRGB = () => {
+    return Math.floor(Math.random() * 255) + 1;
 };
 
-let numDivs = cellsPerRow ** 2;
+button.addEventListener('click', e => {
 
-createDivs(numDivs);
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    let cellsPerRow = prompt("How many cells should be in each row?");
+
+    while ((parseInt(cellsPerRow) > 100) || (parseInt(cellsPerRow) < 1)) {
+        cellsPerRow = prompt("Invalid number of cells! Pick a number between 1 and 100");
+    }
+
+    parseInt(cellsPerRow);
+
+    container.style.gridTemplateColumns = `repeat(${cellsPerRow}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${cellsPerRow}, 1fr)`;
+
+    let createDivs = (num) => {
+        for (let i = 0; i < num; i++) {
+            const div = document.createElement('div');
+            div.classList.add('cell');
+            container.appendChild(div);
+        }
+    };
+
+    let numDivs = cellsPerRow ** 2;
+    createDivs(numDivs);
+
+
+
+    const divs = document.querySelectorAll('.cell');
+    divs.forEach(div => {
+        div.addEventListener('mouseover', e => {
+            div.style.backgroundColor = `rgb(${getRandomRGB()}, ${getRandomRGB()}, ${getRandomRGB()})`;
+            e.stopPropagation();
+        });
+    });
+
+    e.preventDefault();
+});
+
+
+
+
+
+
+
+
