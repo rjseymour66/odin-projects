999024cf8dc9825716d4122a263ad0cbc397224d
rseymour <rjs.github@gmail.com>
2023-11-15 00:14:14 -0500

    reorg to make better sense

--- javascript/tic-tac-toe/app.js
+++ javascript/tic-tac-toe/app.js
@@ -9,36 +9,128 @@ let Gameboard = () => {
     // get the current state of the game
     const getBoard = () => board;
 
-    // a
+    // adds a player's marker to the square
+    const writeMarker = (square, player) => {
+        // get list of empty cells
+        const emptyCells = board.filter((cell) => cell.value === "");
+
+        // if there are no empty cells, return
+        if (!emptyCells.length) return;
+
+        // if the cell is empty, writeMarker for the player
+        square.addPlayerMarker(player);
+    };
+
+    // see what the board looks like
+    const printBoard = () => {
+        // get values from each cell and populate a new array with the values
+        const boardWithCellValues = board.map((cell) => cell.getValue());
+        // print board
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, writeMarker, printBoard };
 };
 
-
-// a cell is a 
+// a cell is a
 let Cell = () => {
     let value = "";
 
-    const addValue = (letter) => {
-        value = letter;
+    // change value of cell to players marker
+    const addPlayerMarker = (marker) => {
+        value = marker;
     };
 
     const getValue = () => value;
 
-    return { addValue, getValue };
+    return { addPlayerMarker, getValue };
 };
 
-let players = () => {
+let players = () => { };
 
+
+let gameController = (
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) => {
+    // build gameboard
+    const board = Gameboard();
+
+    const players = [
+        { name: playerOneName, marker: "X" },
+        { name: playerTwoName, marker: "O" },
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    // const getPlayerInput = () => prompt();
+
+    // print the board to the console and list player turn
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const checkForWinner = (gameBoard, playerMarker) => {
+
+        let board = gameBoard.getBoard();
+
+        const playerCells = board.filter((cell) => cell.getValue() === playerMarker);
+
+        const winningCombos = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7],
+        ];
+
+
+        let result = winningCombos.forEach(array => {
+            array.every(el => playerCells.includes(el));
+        });
+
+        return result;
+    };
+
+    const playerWins = () => {
+        console.log(`${getActivePlayer().name} wins!`);
+    };
+
+    const playRound = (square) => {
+        // add a marker for the current player
+        console.log(
+            `Writing ${getActivePlayer().name}'s mark into square ${square}...`
+        );
+        board.writeMarker(square, getActivePlayer().marker);
+
+        // check winning combos
+        if (checkForWinner(board, activePlayer.marker)) {
+            playerWins();
+        } else {
+            console.log('No winners!');
+            switchPlayerTurn();
+            printNewRound();
+
+        }
+        printNewRound();
+    };
+
+    // Begin the game
+    printNewRound();
+    let playerSelection = getPlayerInput();
+    playRound(playerSelection);
+
+    return { playRound, getActivePlayer };
 };
 
-
-
-let gameController = () => {
-
-    // 1 2 3
-    // 4 5 6
-    // 7 8 9
-
-    // winning combinations
-
-
-};
+const game = gameController();
---
+++ javascript/tic-tac-toe/script.js
@@ -0,0 +1,67 @@
+let Square = () => {
+
+    // default value for each Square
+    let value = "";
+
+    // change the value of the Square to a players marker
+    const addPlayerMarker = (playerMarker) => {
+        value = playerMarker;
+    };
+
+    // get the value of the Square
+    const getValue = () => value;
+
+    return { addPlayerMarker, getValue };
+};
+
+let Gameboard = () => {
+    // create board array
+    let board = [];
+
+    // create array with 9 elements, each element stores a Square
+    for (let i = 0; i < 9; i++) {
+        board[i] = Square();
+    }
+
+    // get current state of board
+    const getBoard = () => board;
+
+    // make sure the Square is empty and then write the
+    // players marker in the Square
+    const acceptPlayerMarker = (selectedSquare, playerMarker) => {
+        // get list of empty Squares
+        const emptySquares = board.filter(square => square.getValue() === "");
+
+        // if no empty Squares, return
+        if (!emptySquares.length) return;
+
+        // if the Square is empty, add the player's marker
+        selectedSquare.addPlayerMarker(playerMarker);
+    };
+
+    // log current board to console
+    const printBoard = () => {
+        // create new array with Square values
+        const boardWithSquareVals = board.map(square => square.getValue());
+
+        // log board
+        console.log(boardWithSquareVals);
+    };
+
+    return { getBoard, acceptPlayerMarker, printBoard };
+};
+
+let gameController = (playerOneName = "Player one", playerTwoName = "Player two") => {
+    // create a new Gameboard
+    const newBoard = Gameboard();
+
+    // create player objects with assigned markers
+
+    // assign active player
+
+    // func that switches active player
+
+    // func that returns active player
+
+
+};
