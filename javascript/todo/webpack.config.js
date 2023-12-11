4ec0632c09e043df36a05e57097b2cf174a8e338
rseymour <rjs.github@gmail.com>
2023-12-10 21:11:42 -0500

    todo project setup

---
+++ javascript/todo/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const json5 = require('json5');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.json5$/i,
+                type: 'json',
+                parser: {
+                    parse: json5.parse,
+                },
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Restaurant | Odin',
+            filename: 'index.html',
+            // inject: 'body',
+            inject: 'head',
+        })
+    ]
+};
